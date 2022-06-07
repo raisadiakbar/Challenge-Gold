7b5aafd9198dd0be9d77163160f030690d8508a8
Rais Adi Akbar <101171335+raisadiakbar@users.noreply.github.com>
2022-06-07 17:13:33 +0900

    Delete orders.routes.js

--- orders.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const orderController = require('../controllers/orders.controller');
-
-router.get('/', orderController.getAllorder);
-router.get('/:id', orderController.getByID);
-router.put('/:id', orderController.updateorder);
-router.delete('/:id', orderController.deleteOrder);
-
-module.exports = router;
